57c52ff6f585ace18aebc4f2af971b04e097fd60
Johncarlos <j69875560@gmail.com>
2026-04-28 09:41:18 +0300

    declaring the variables for the days

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document
   .getElementById("akanForm")
   .addEventListener("submit", function (event) {
-    event.preventDefault();}
+    event.preventDefault();
+
+
+    let day = parseInt(document.getElementById("day").value);
+    let month = parseInt(document.getElementById("month").value);
+    let year = parseInt(document.getElementById("year").value);
+    let gender = document.getElementById("gender").value;
+  }
