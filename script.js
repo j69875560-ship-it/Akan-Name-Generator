38fd7b09976cd41d6b68c6f160df066852b5b227
Johncarlos <j69875560@gmail.com>
2026-04-28 09:44:10 +0300

    validating months and days

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 document
   .getElementById("akanForm")
   .addEventListener("submit", function (event) {
-    event.preventDefault();
+    event.preventDefault(); // stop page refresh
 
-
+    // 1. GET INPUT VALUES
     let day = parseInt(document.getElementById("day").value);
     let month = parseInt(document.getElementById("month").value);
     let year = parseInt(document.getElementById("year").value);
     let gender = document.getElementById("gender").value;
+
+    // 2. VALIDATION
+    if (day < 1 || day > 31 || month < 1 || month > 12 || gender === "") {
+      alert("Please enter valid data!");
+      return;
+    }
   }
